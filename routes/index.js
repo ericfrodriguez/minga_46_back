a7ad7f28f248c38c82d288900f9aaed9457bc40f
Eric Rodriguez <feric.rodriguez@gmail.com>
2023-04-21 11:50:20 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// var express = require('express');
+import express from 'express'
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+export default router;

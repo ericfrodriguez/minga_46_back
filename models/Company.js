c01ec5d1190bd9105f74ff21cd0bfcf45d2063f4
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-04-21 16:06:07 -0300

    b-04 (#4)

---
+++ models/Company.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+let schema = new mongoose.Schema({
+    name: { type:String,required:true },
+    logo: { type:String,required:true },
+    website: { type:String,required:true },
+    description: { type:String,required:true },
+    active: { type:Boolean,required:true },
+    user_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'users',
+        required: true
+    }
+},{
+    timestamps: true
+})
+let collection = 'companies'
+
+let Company = mongoose.model(schema,collection)
+export default Company
---
+++ models/data/companies.js
@@ -0,0 +1,17 @@
+export let companies = [{
+    name: "Peace",
+    logo: "https://i.postimg.cc/kgQgTVVw/W4-Wjxsq-ZW-1300x655-1.jpg",
+    website: "https://purpleteam-minga.com",
+    description: "The founding members are Luciana Rovere, Camilo Sanchez and Gustavo Espinosa. Later, Patricio Gimenez joined the team.",
+    user_id: "id de usuario 7",
+    active: true,
+    user_id: "peace@team.com.ar"
+},{
+    name: "Digital",
+    logo: "https://i.postimg.cc/CxqqSLRf/Empresa-digital-jpg-width-600-name-Empresa-digital.jpg",
+    website: "https://blueteam-minga.com",
+    description: "The founding members are Candela Cañete, Alejandro Dacunto, Valentín Caceres, Cristian Elfante y Pablo Muñiz.",
+    user_id: "id de usuario 8",
+    active: true,
+    user_id: "digital@team.com.ar"
+}]

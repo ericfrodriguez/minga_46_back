1a668494f7d3f1f75dffcd52ae8917c9d15bd1f7
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-04-24 12:50:03 -0300

    read (#8)

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -1,6 +1,19 @@
-let read = (req,res,next)=> res.status(200).render('index',{
-    title: '/CATEGORIES',
-    subtitle: 'endpoints of categories'
-})
+import Category from './../../models/Category.js'
+
+let read = async(req,res,next)=> {          //la funcion controladora debe ser asincrona para poder esperar la respuesta de MONGO
+    try {                                   //utilizo la sintaxis de try/catch para intentar algo y catchear lo errores que puedan surgir
+        let all = await Category.find()     //utilizo el método find() para buscar todos los recursos del modelo (que en este caso es CATEGORY)
+        return res.status(200)              //configuro la respuesta que le tengo que enviar al cliente (front)
+            .json({
+                categories: all
+            })
+    } catch(error) {
+        console.log(error)
+        return res.status(400)
+            .json({
+                error: 'ha ocurrido un error!'
+            })
+    }
+}
 
 export default read

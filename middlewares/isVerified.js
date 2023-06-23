9030af9e8b16f2b0c2d66a20f8f5e781295c0346
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-06-23 10:04:22 -0300

    reset (#19)

--- middlewares/isVerified.js
+++
@@ -1,9 +0,0 @@
-async function accountHasBeenVerified(req,res,next) {
-    if (req.user.is_verified) {
-        return next()
-    }
-    return res.status(400).send('has not been verified!')
-    //enviar una respuesta con json
-}
-
-export default accountHasBeenVerified
--- middlewares/notFound.js
+++
@@ -1,7 +0,0 @@
-import createHttpError from "http-errors";
-
-const notFound = (req, res, next) => {
-    next(createHttpError(404, 'La ruta no existe'))
-}
-
-export default notFound;

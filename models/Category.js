efded0e3fa78c28648b2f2e2fb54cb72a378035b
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-26 12:41:18 -0300

    categories

---
+++ models/Category.js
@@ -0,0 +1,17 @@
+import { Schema,model,Types } from "mongoose"
+
+let collection = 'categories'
+let schema = new Schema({
+    name: { type:String,required:true },
+    color: { type:String,required:true },
+    hover: { type:String,required:true },
+    description: { type:String,required:true },
+    cover_photo: { type:String,required:true },
+    character_photo: { type:String,required:true },
+    admin_id: { type:Types.ObjectId,ref:'users',required:true }
+},{
+    timestamps:true
+})
+
+const Category = model(collection,schema)
+export default Category

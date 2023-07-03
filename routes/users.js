387281cafbc8d6224d6eadb55826cb2411a12fc6
ignacioborraz <ignacioborraz@hotmail.com>
2023-07-03 13:54:45 -0300

    signin

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,18 @@
 import { Router } from "express"
-import create from "../controllers/users/create.js";
+import register from "../controllers/users/register.js";
+import signin from "../controllers/users/signin.js";
 
 import validator from "../middlewares/validator.js";
 import userSignup from "../schemas/auth/signup.js";
 import accountExists from "../middlewares/accountExists.js";
 import createHash from "../middlewares/createHash.js";
+import accountNotExists from "../middlewares/accountNotExists.js"
+import passwordIsOk from "../middlewares/passwordIsOk.js";
+import generateToken from "../middlewares/generateToken.js";
 
 const auth_router = Router()
 
-//auth_router.get()
-auth_router.post('/register',validator(userSignup), accountExists, createHash, create);
+auth_router.post('/register',validator(userSignup), accountExists, createHash, register)
+auth_router.post('/signin'/* validator(schemaSignin) */, accountNotExists, passwordIsOk, generateToken, signin)
 
 export default auth_router

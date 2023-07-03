8ed76344b16c931c738171162b90b50c83766559
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-07-03 13:55:25 -0300

    signin (#28)

---
+++ controllers/users/signin.js
@@ -0,0 +1,22 @@
+import User from "../../models/User.js"
+
+export default async(req,res,next)=> {
+    try {
+        let one = await User.findOneAndUpdate(
+            { email:req.body.email },   //objeto con las propiedades que se deben buscar (en este caso por email)
+            { online:true },            //objeto con las propeidades a modificar del documento encontrado (en este caso ONLINE)
+            { new:true }                //objeto de configuración, si está en TRUE devuelve el objeto MODIFICADO
+        )
+        delete one.password
+        return res.status(200).json({
+            success:true,
+            message:'user signed in!',
+            reponse: {
+                user:one,
+                token:req.token
+            }
+        })
+    } catch (error) {
+        return next()
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -5,33 +5,11 @@ import manga_router from './mangas.js'
 import chapter_router from './chapters.js'
 import auth_router from './users.js'
 
-import middleware1 from '../middlewares/middleware1.js'
-import middleware2 from '../middlewares/middleware2.js'
-
-
 let router = express.Router()
-
-// const printYear = (req, res, next) => {
-//   console.log('Time (indexRouter): ', new Date().getFullYear()); //* Imprimir Time: 2023
-
-//   next();
-// }
-
-// router.use(printYear)
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 })
-
-// router.get('/pepito', middleware1, middleware2, (req, res, next) => {
-//   console.log(req)
-
-//   res.json({
-//     midd1: req.midd1,
-//     midd2: req.midd2
-//   });
-// })
-
 
 router.use('/authors',author_router)        //obligo al enrutador principal a usar las rutas de authors con la palabrita /authors
 router.use('/categories',category_router)   //obligo al enrutador principal a usar las rutas de categories con la palabrita /categories

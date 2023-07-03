233da9c5a855f24e423009fd18a890e1e3c5156f
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-07-03 16:49:09 -0300

    signout (#29)

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import register from "../controllers/users/register.js";
 import signin from "../controllers/users/signin.js";
+import signout from "../controllers/users/signout.js";
 
 import validator from "../middlewares/validator.js";
 import userSignup from "../schemas/auth/signup.js";
@@ -9,10 +10,12 @@ import createHash from "../middlewares/createHash.js";
 import accountNotExists from "../middlewares/accountNotExists.js"
 import passwordIsOk from "../middlewares/passwordIsOk.js";
 import generateToken from "../middlewares/generateToken.js";
+import passport from "../middlewares/passport.js";
 
 const auth_router = Router()
 
 auth_router.post('/register',validator(userSignup), accountExists, createHash, register)
 auth_router.post('/signin'/* validator(schemaSignin) */, accountNotExists, passwordIsOk, generateToken, signin)
+auth_router.post('/signout',passport.authenticate('jwt',{ session:false }),signout)
 
 export default auth_router
